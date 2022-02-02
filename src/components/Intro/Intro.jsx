cabe3ce0b64b8ea8fea19cfa5264ccff38f36448
Hiro Protagonist <shdesai118@gmail.com>
2022-02-02 09:19:47 -0700

    testing change

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 
 export default function Intro() {
   return <div className='intro'>
-    <p>Intro</p>
+    <p>
+          My name is Siddharth Desai with a degree in Computer Science from NJIT. I kick started my career at Lockheed Martin where I worked in the roles of Software Engineer and Systems Engineer. At Lockheed Martin, I worked on many complex and exciting projects SCOT-Aegis which simulated the environment for operators in ships that used the Aegis Combat system. I learned tremendously about different weapon systems, radar, sensors, luminator trajectory mathematics and much more. The complex work at Lockheed helped me develop a strong work ethic and improve my overall knowledge on how to be a better engineer. I am currently working at PSEG based out of NJ and support various applications  such as FileNet, McLaren, Axway, iManage and recently was given a new task to automate manual workflows, controls and upload data to a cloud environment. I have learned extensively about servers, security and network infrastructure. 
+         
+          A little more about me is that I have a dog named Hiro and a beautiful wife named Neha. My wife recently started a job in Colorado so we moved here and I am working fully remote. Some of my hobbies are reading manga, watching anime, and studying different topics such as philosophy, hydroponics, and astronomy. I also love working out, hiking, playing sports and of course playing video games.
+        </p>
   </div>;
 }
+<div class="alert alert-dismissible alert-light">
+  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+  <strong>Heads up!</strong> This <a href="#" class="alert-link">alert needs your attention</a>, but it's not super important.
+</div>
