2e23108049109a0cc11c412078b82dfa0dc0975a
Hiro Protagonist <shdesai118@gmail.com>
2022-02-02 12:46:33 -0700

    fix center

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -12,17 +12,18 @@ export default function Intro() {
         </div>
       </div>
     </div>
-    <div className="row" >
-      <div className='col-sm'>
-        <div className="cardForm">
-          <div className="card border-primary mb-3" style={{ maxWidth: '20rem' }}>
-            <div className="card-header">Header</div>
-            <div className="card-body">
-              <h4 className="card-title">Primary card title</h4>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className='container'>
+      <div className="row" >
+        <div className='col-sm'>
+          <div className="cardForm">
+            <div className="card border-primary mb-3" style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
