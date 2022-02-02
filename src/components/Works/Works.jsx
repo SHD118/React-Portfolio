f0ac203790cd9a7b5dc63d26697a414897ea8cd0
Hiro Protagonist <shdesai118@gmail.com>
2022-02-02 13:42:02 -0700

    added works

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -4,6 +4,72 @@ import React from 'react';
 
 export default function Works() {
   return <div className='works'>
-    <p>Works</p>
-  </div>;
+    <div>
+      <div className='cardstyle'>
+        <div className='row '>
+          <div class="col">
+
+            <div className="card border-primary mb-3 " style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div className="card border-secondary mb-3" style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Secondary card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div className="card border-success mb-3" style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Success card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div></div>
+
+      </div>
+      <div className='cardstyle'>
+        <div className='row'>
+          <div class="col">
+            <div className="card border-danger mb-3" style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Danger card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div className="card border-warning mb-3" style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Warning card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div className="card border-info mb-3" style={{ maxWidth: '20rem' }}>
+              <div className="card-header">Header</div>
+              <div className="card-body">
+                <h4 className="card-title">Info card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
 }
