f6c36ee9557cf286851fa0a4f190c79a519d3f2b
Hiro Protagonist <shdesai118@gmail.com>
2022-02-02 16:57:37 -0700

    added styling

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,20 +1,26 @@
 import './Intro.css'
 import React from 'react';
-// import image from "././public/me.png"
+import image from "../../images/me.png";
+
 
 export default function Intro() {
   return <div className='intro'>
 
-    <div className="row" >
+    {/* <div className="row" >
       <div className='col-sm'>
         <div className="banner">
-          <img src="./sid.jpg" alt="Logo" />
+          <img src={image} style={{ width: '20%', height: '20%', float: 'left', marginTop: '5%', marginLeft: '5%' }} alt="Logo" />
         </div>
       </div>
-    </div>
+    </div> */}
     <div className='container'>
       <div className="row" >
         <div className='col-sm'>
+          <div className="banner">
+            <img src={image} style={{ width: '100%', height: '80%', float: 'left', marginTop: '5%', marginLeft: '5%' }} alt="Logo" />
+          </div>
+        </div>
+        <div className='col-sm'>
           <div className="cardForm">
             <div className="card border-primary mb-3" style={{ maxWidth: '20rem' }}>
               <div className="card-header">Header</div>
@@ -48,68 +54,31 @@ export default function Intro() {
           </div>
         </div>
       </div>
+
+
+
+    </div>;
+
+    <div class="container bars">
+      <div class="row">
+        <div>
+          <div className="progress">
+            <div className="progress-bar bg-success" role="progressbar" style={{ width: '25%' }} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100} />
+          </div>
+          <div className="progress">
+            <div className="progress-bar bg-info" role="progressbar" style={{ width: '50%' }} aria-valuenow={50} aria-valuemin={0} aria-valuemax={100} />
+          </div>
+          <div className="progress">
+            <div className="progress-bar bg-warning" role="progressbar" style={{ width: '75%' }} aria-valuenow={75} aria-valuemin={0} aria-valuemax={100} />
+          </div>
+          <div className="progress">
+            <div className="progress-bar bg-danger" role="progressbar" style={{ width: '100%' }} aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} />
+          </div>
+        </div>
+      </div>
     </div>
-
-
-  </div>;
+  </div>
 }
 
 
-{/* <div>
-<div className="card border-primary mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-secondary mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Secondary card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-success mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Success card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-danger mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Danger card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-warning mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Warning card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-info mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Info card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-light mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Light card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div className="card border-dark mb-3" style={{maxWidth: '20rem'}}>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Dark card title</h4>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-</div> */}
+
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,7 +1,7 @@
 import './Works.css'
 
 import React from 'react';
-
+import image from "../../images/Weather.gif"
 export default function Works() {
   return <div className='works'>
     <div>
@@ -14,6 +14,7 @@ export default function Works() {
               <div className="card-body">
                 <h4 className="card-title">Primary card title</h4>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <img src={image} alt="hi"></img>
               </div>
             </div>
           </div>
